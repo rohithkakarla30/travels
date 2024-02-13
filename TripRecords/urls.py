db7bbf15ff8e0c5e4e7137fab287fa0b97742efb
AmarSabbisetti <amarnadh.s@consultadd.com>
2024-02-13 06:52:19 -0500

    update1

--- TripRecords/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import hello, AddUserPackage
-urlpatterns=[
-    path('',hello),
-    path('add/<int:package>/',AddUserPackage.as_view(),name='AddUserPackage')
-]
--- core/urls.py
+++ core/urls.py
@@ -26,9 +26,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('',include(('users.urls','users'),namespace='users')),
     path('api/packages/',include(('packages.urls','packages'),namespace='packages')),
-    path('api/triprecords/',include(('TripRecords.urls','TripRecords'),namespace='triprecords')),
+    #path('api/triprecords/',include(('TripRecords.urls','TripRecords'),namespace='triprecords')),
     path('auth/', include('rest_framework.urls'), name='rest_framework'),
-    #path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    #path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+#path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+#path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
